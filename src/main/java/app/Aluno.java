3557981b657736a0ed58a8ca63f71f70550a6184
Thais Zimmer <165618062+thaisczimmer@users.noreply.github.com>
2025-08-20 01:42:12 +0000

    aula19/08marcocodigoaltaperfomance

---
+++ src/main/java/app/Aluno.java
@@ -0,0 +1,15 @@
+package app;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+
+public class Aluno {
+    private long id;
+    private String nome;
+    private int idade; 
+}
+
--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -3,5 +3,13 @@ package app;
 public class PrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Olá Gradle!!!");
+
+        Aluno a1 = new Aluno();
+        a1.setID(1);
+        a1.setNome("Marco");
+        a1.setIdade(45);
+
+        System.out.println(a1.getNome());
+
     }
 }
